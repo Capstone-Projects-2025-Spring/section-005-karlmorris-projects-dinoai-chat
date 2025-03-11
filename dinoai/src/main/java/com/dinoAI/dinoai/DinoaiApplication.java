e5d3d404d20f440f80bf6d6323c20f41f17ec7f7
Jiacheng He <tuo44998@temple.edu>
2025-03-11 13:16:55 -0400

    initialize the project

---
+++ dinoai/src/main/java/com/dinoAI/dinoai/DinoaiApplication.java
@@ -0,0 +1,13 @@
+package com.dinoAI.dinoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoaiApplication.class, args);
+	}
+
+}
