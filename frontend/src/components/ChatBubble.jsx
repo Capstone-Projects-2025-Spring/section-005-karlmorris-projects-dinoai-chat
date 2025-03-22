1cd039991086f771221cb543f53776b4c7c7aa10
Jiacheng He <tuo44998@temple.edu>
2025-03-21 22:03:48 -0400

    frontend: small UI fix in Chat Page

--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -1,9 +1,9 @@
 export default function ChatBubble({ message, isUser }) {
-  return (
-      <div className={`chat ${isUser ? "chat-end" : "chat-start"}`}>
-          <div className={`chat-bubble ${isUser ? "chat-bubble-info" : "chat-bubble-"}`}>
-              {message}
-          </div>
-      </div>
-  );
+    return (
+        <div className={`chat ${isUser ? "chat-end" : "chat-start"}`}>
+            <div className={`chat-bubble break-words ${isUser ? "bg-lime-200" : ""}`}>
+                {message}
+            </div>
+        </div>
+    );
 }
--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,10 +1,10 @@
 import ChatBubble from "./ChatBubble";
 export default function ChatWindow({ messages }) {
-  return (
-      <div className="flex flex-col p-4 h-[500px] overflow-y-auto">
-          {messages.map((msg, index) => (
-              <ChatBubble key={index} message={msg.text} isUser={msg.isUser} />
-          ))}
-      </div>
-  );
+    return (
+        <div className="flex flex-col p-4 h-auto">
+            {messages.map((msg, index) => (
+                <ChatBubble key={index} message={msg.text} isUser={msg.isUser} />
+            ))}
+        </div>
+    );
 }
