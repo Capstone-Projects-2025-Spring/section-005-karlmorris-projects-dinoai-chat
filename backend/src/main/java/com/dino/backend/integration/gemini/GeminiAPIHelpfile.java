c6c2298d1cb34d3a5003c9173b4d4d0a01caae32
rosegly <tun60623@temple.edu>
2025-04-07 23:41:28 -0400

    Add API controller and help file

---
+++ backend/src/main/java/com/dino/backend/integration/gemini/GeminiAPIHelpfile.java
@@ -0,0 +1,13 @@
+package com.dino.backend.integration.gemini;
+
+public class InputRequest {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
