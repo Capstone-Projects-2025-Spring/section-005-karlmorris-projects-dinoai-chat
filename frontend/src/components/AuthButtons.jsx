855b59026a2e4e0aa18c0451bedb7a9fdcbaef3d
Yusef Tohamy <tuo13985@temple.edu>
2025-04-08 13:09:20 -0400

    Login button only appears if user is logged out

--- frontend/src/components/AuthButtons.jsx
+++ frontend/src/components/AuthButtons.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Button from "./Button";
+import { checkAuthWithBackend } from "../utils/auth";
 
 export default function AuthButtons() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const verify = async () => {
+            const valid = await checkAuthWithBackend();
+            setIsLoggedIn(valid);
+        };
+        verify();
+    }, []);
+
     return (
         <div className="flex gap-x-2">
             <NavLink to="/upgrade">
@@ -10,11 +22,13 @@ export default function AuthButtons() {
                 </Button>
             </NavLink>
 
-            <NavLink to="/login">
-                <Button variant="secondary">
-                    Log in
-                </Button>
-            </NavLink>
+            {!isLoggedIn && (
+                <NavLink to="/login">
+                    <Button variant="secondary">
+                        Log in
+                    </Button>
+                </NavLink>
+            )}
         </div>
-    )
+    );
 }
